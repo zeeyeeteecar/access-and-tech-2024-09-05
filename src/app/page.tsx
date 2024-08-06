6d8c226789974ea25ad0ceddd65fdb06a6831e89
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-08-06 14:24:27 -0700

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function page() {
           return (
             <>
               <li>{JSON.stringify(user)}</li>
-              <div className="w-full"></div>
+              <div className="w-full">ttttttt</div>
             </>
           );
         })}
