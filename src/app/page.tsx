51ef314538e70210ef3fd391ad367259b8ba0bc4
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-08-06 14:29:32 -0700

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 export default async function page() {
   const prisma = new PrismaClient();
 
-  const users = await prisma.tBEventReg_SEEE_2018_VenderInfo.findMany();
+  const users = await prisma.tBEventReg_SEEE_2018_VenderInfo.findMany({});
 
   return (
     <>
