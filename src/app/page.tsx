cf8b46184ec0fe7f1d8ca5f6aec50af3afb4fd41
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-08-06 13:44:15 -0700

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,5 +7,17 @@ export default async function page() {
 
   const users = await prisma.tBEventReg_SEEE_2018_VenderInfo.findMany();
 
-  return <>{JSON.stringify(users)}</>;
+  return (
+    <>
+      {users &&
+        users.map((user: any, key: number) => {
+          return (
+            <>
+              <li>{JSON.stringify(user)}</li>
+              <div className="w-full"></div>
+            </>
+          );
+        })}
+    </>
+  );
 }
